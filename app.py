c8e8d41a492e8f348171ded143300b7232f4db5d
Dark Coder <codewithdark90@gmail.com>
2024-09-12 09:44:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import asyncio
 from datetime import datetime
 from dotenv import load_dotenv
 from utility.logging import log_response
-from utility.script_generator.py import generate_script
+from utility.script_generator import generate_script
 from utility.audio_generator import generate_audio
 from utility.timed_captions_generator import generate_timed_captions
 from utility.image_generator import generate_image_prompts, generate_images
